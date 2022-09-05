5bcb7d53e7ada18d87a550d2ac365f6f8cd92195
윤혁준 <gurwns1540@gmail.com>
2022-09-05 22:50:32 +0900

    테스트 주도 개발 9장 완료

--- src/main/java/com/hjyoon/study/numberguessing/model/AppModel.java
+++ src/main/java/com/hjyoon/study/numberguessing/model/AppModel.java
@@ -1,5 +1,7 @@
 package com.hjyoon.study.numberguessing.model;
 
+import java.util.stream.Stream;
+
 public class AppModel {
 
     interface Processor {
@@ -9,16 +11,19 @@ public class AppModel {
     private final static String NEW_LINE = System.lineSeparator();
     private final static String SELECT_MODE_MSG = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
     private boolean completed;
-    private String output;
+//    private String output;
+    private final StringBuffer outputBuffer;
+    
     private final PositiveIntegerGenerator positiveIntegerGenerator;
     private Processor processor;
 
     public AppModel(PositiveIntegerGenerator positiveIntegerGenerator) {
         this.positiveIntegerGenerator = positiveIntegerGenerator;
         completed = false;
-        output = SELECT_MODE_MSG;
+//        output = SELECT_MODE_MSG;
 //        answer = positiveIntegerGenerator.generateLessThanOrEqualToHundred();
         processor = this::processModeSelection;
+        outputBuffer = new StringBuffer(SELECT_MODE_MSG);
     }
 
     //완료여부
@@ -28,7 +33,9 @@ public class AppModel {
 
     //출력 및 버퍼삭제
     public String flushOutput() {
-        return output;
+        String s = outputBuffer.toString();
+        outputBuffer.setLength(0);
+        return s;
     }
 
     //
@@ -43,29 +50,72 @@ public class AppModel {
 //        }
     }
 
+    private void print(String message) {
+        outputBuffer.append(message);
+    }
+
+    private void println(String message) {
+        outputBuffer.append(message).append(NEW_LINE);
+    }
+
     private Processor processModeSelection(String input) {
         int i = Integer.parseInt(input);
         if (i == 1) {
-            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100." + NEW_LINE + "Enter your guess: ";
+            println("Single player game");
+            println("I'm thinking of a number between 1 and 100.");
+            print("Enter your guess: ");
             int answer = positiveIntegerGenerator.generateLessThanOrEqualToHundred();
             return getSinglePlayerProcessor(answer, 1);
+        } else if (i == 2) {
+            println("Multiplayer game");
+            print("Enter player names seperated with comas: ");
+            return startMultiplayerGame();
         } else {
             completed = true;
             return null;
         }
     }
 
+    private Processor startMultiplayerGame() {
+        return input -> {
+            Object[] players = Stream.of(input.split(",")).map(String::trim).toArray();
+            println("I'm thinking of a number between 1 and 100.");
+            int answer = positiveIntegerGenerator.generateLessThanOrEqualToHundred();
+            return getMultiPlayerProcessor(players, answer, 1);
+        };
+    }
+
+    private Processor getMultiPlayerProcessor(Object[] players, int answer, int tries) {
+        outputBuffer.append(" Enter ").append(players[(tries - 1) % players.length]).append("'s guess: ");
+        return input -> {
+            int guess = Integer.parseInt(input);
+
+            if(guess < answer) {
+                outputBuffer.append(players[(tries - 1) % players.length]).append("'s guess is too low.").append(NEW_LINE);
+            } else if(guess > answer) {
+                outputBuffer.append(players[(tries - 1) % players.length]).append("'s guess is too high.").append(NEW_LINE);
+            } else {
+                outputBuffer.append("Correct! ").append(players[(tries - 1) % players.length]).append(" wins.").append(NEW_LINE);
+                outputBuffer.append(SELECT_MODE_MSG);
+                return this::processModeSelection;
+
+            }
+
+            return getMultiPlayerProcessor(players, answer, tries + 1);
+        };
+    }
+
     private Processor getSinglePlayerProcessor(int answer, int tries) {
         return input -> {
             int i = Integer.parseInt(input);
             if (i < answer) {
-                output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+                outputBuffer.append("Your guess is too low.").append(NEW_LINE).append("Enter your guess: ");
                 return getSinglePlayerProcessor(answer, tries + 1);
             } else if (i > answer) {
-                output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
+                outputBuffer.append("Your guess is too high.").append(NEW_LINE).append("Enter your guess: ");
                 return getSinglePlayerProcessor(answer, tries + 1);
             } else {
-                output = "Correct! " + tries + ((tries == 1) ? (" guess.") : (" guesses.")) + NEW_LINE + SELECT_MODE_MSG;
+                outputBuffer.append("Correct! ").append(tries).append((tries == 1) ? (" guess.") : (" guesses.")).append(NEW_LINE).append(SELECT_MODE_MSG);
                 return this::processModeSelection;
             }
         };
--- src/test/java/com/hjyoon/study/numberguessing/AppModelTest.java
+++ src/test/java/com/hjyoon/study/numberguessing/AppModelTest.java
@@ -220,6 +220,240 @@ public class AppModelTest {
 
         //then
         assertThat(actual).startsWith(expected);
+    }
 
+    @Test
+    void correctlyPrintMultiPlayerGameSetupMessage() {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+
+
+        //when
+        appModel.flushOutput();
+        appModel.processInput("2");
+
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).isEqualTo("Multiplayer game" + NEW_LINE + "Enter player names seperated with comas: ");
+
+    }
+
+    @Test
+    void correctlyPrintMultiPlayerStartMessage() {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+
+        //when
+        appModel.processInput("2");
+        appModel.flushOutput();
+
+        appModel.processInput("Foo, Bar");
+
+        String actual = appModel.flushOutput();
+        //then
+
+        assertThat(actual).startsWith("I'm thinking of a number between 1 and 100" + NEW_LINE);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void correctlyPromptsFirstPlayerName(String player1, String player2, String player3) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.flushOutput();
+
+        appModel.processInput(String.join(", ", player1, player2, player3));
+
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).endsWith("Enter " + player1 + "'s guess: ");
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void correctlyPromptsSecondPlayerName(String player1, String player2, String player3) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput(String.join(", ", player1, player2, player3));
+        appModel.flushOutput();
+
+        appModel.processInput("10");
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).endsWith("Enter " + player2 + "'s guess: ");
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void correctlyPromptsThirdPlayerName(String player1, String player2, String player3) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput(String.join(", ", player1, player2, player3));
+        appModel.processInput("90");
+        appModel.flushOutput();
+
+        appModel.processInput("90");
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).endsWith("Enter " + player3 + "'s guess: ");
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void correctlyRoundsPlayers(String player1, String player2, String player3) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput(String.join(", ", player1, player2, player3));
+        appModel.processInput("10");
+        appModel.processInput("10");
+        appModel.flushOutput();
+
+        appModel.processInput("10");
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).endsWith("Enter " + player1 + "'s guess: ");
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "50, 40, 1, Foo", "30, 29, 2, Bar" })
+    void correctlyPrintTooLowMessageInMultiPlayerGame(int answer, int guess, int fails, String lastPlayer) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(answer));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput("Foo, Bar, Baz");
+        for (int i = 0; i < fails - 1; i++) {
+            appModel.processInput(Integer.toString(guess));
+        }
+        appModel.flushOutput();
+        appModel.processInput(Integer.toString(guess));
+
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).contains(lastPlayer + "'s guess is too low." + NEW_LINE);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "50, 60, 1, Foo", "9, 81, 2, Bar" })
+    void correctlyPrintTooHighMessageInMultiPlayerGame(int answer, int guess, int fails, String lastPlayer) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(answer));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput("Foo, Bar, Baz");
+        for (int i = 0; i < fails - 1; i++) {
+            appModel.processInput(Integer.toString(guess));
+        }
+        appModel.flushOutput();
+        appModel.processInput(Integer.toString(guess));
+
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).contains(lastPlayer + "'s guess is too high." + NEW_LINE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100})
+    void correctlyPrintCorrectMessageInMultiPlayerGame(int answer) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(answer));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput("Foo, Bar, Baz");
+        appModel.flushOutput();
+
+        int guess = answer;
+        appModel.processInput(Integer.toString(guess));
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).startsWith("Correct! ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "0, Foo", "1, Bar", "2, Baz", "99, Foo", "100, Bar" })
+    void correctlyPrintWinnerIfMultiPlayerGameFinished(int fails, String winner) {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput("Foo, Bar, Baz");
+        for (int i = 0; i < fails; i++) {
+            appModel.processInput("30");
+        }
+        appModel.flushOutput();
+        appModel.processInput("50");
+
+        String actual = appModel.flushOutput();
+
+        //then
+        assertThat(actual).contains(winner + " wins." + NEW_LINE);
+    }
+
+    @Test
+    void printSSelectModeMessageIfMultiPlayerGameFinished() {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput("Foo, Bar, Baz");
+        appModel.flushOutput();
+        appModel.processInput("50");
+
+        String actual = appModel.flushOutput();
+        //then
+
+        assertThat(actual).endsWith("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ");
+
+    }
+
+    @Test
+    void returnsToModeSelectionIfMultiPlayerGameFinished() {
+        //given
+        AppModel appModel = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        //when
+        appModel.processInput("2");
+        appModel.processInput("Foo, Bar, Baz");
+        appModel.processInput("20");
+        appModel.processInput("50");
+        appModel.processInput("3");
+
+        boolean actual = appModel.isCompleted();
+        //then
+
+        assertThat(actual).isTrue();
     }
 }
